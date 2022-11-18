77cd59189602b6b12db4afec8a98c1020167d80f
Darin Crawmer <darindev1000@gmail.com>
2022-11-17 19:29:17 -0800

    generate tile groups

--- Assets/SCR_PerlinNoiseMap.cs
+++ Assets/SCR_PerlinNoiseMap.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class SCR_PerlinNoiseMap : MonoBehaviour
 {
+    Dictionary<int, GameObject> tileset;
+    Dictionary<int, GameObject> tileGroups;
+
+    public GameObject prefabDirt;
+    public GameObject prefabGrass;
+    public GameObject prefabGrassPlant;
+    public GameObject prefabBush;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateTileset();
+        CreateTileGroups();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CreateTileset()
     {
-        
+        tileset = new()
+        {
+            { 0, prefabDirt },
+            { 1, prefabGrass },
+            { 2, prefabGrassPlant },
+            { 3, prefabBush }
+        };
+    }
+
+    void CreateTileGroups()
+    {
+        tileGroups = new();
+        foreach (KeyValuePair<int, GameObject> prefabPair in tileset)
+        {
+            GameObject tileGroup = new(prefabPair.Value.name);
+            tileGroup.transform.parent = gameObject.transform;
+            tileGroup.transform.localPosition = Vector3.zero;
+            // Add tileGroup to tileGroups
+            tileGroups.Add(prefabPair.Key, tileGroup);
+        }
     }
 }
